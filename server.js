dc00af8e282dcf1221a05c7a3c032de3e3bd9e8d
Amanda Angela Soenoko <75839443+Amdsnk@users.noreply.github.com>
2025-03-04 21:28:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -145,5 +145,4 @@ process.on("uncaughtException", (error) => {
 
 process.on("unhandledRejection", (reason, promise) => {
   console.error("Unhandled Rejection at:", promise, "reason:", reason)
-  // Application specific logging, throwing an error, or other logic here
 })
