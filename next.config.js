7a4a763b4933734d941b84d0e672ba1b4ab33f88
Amanda Angela Soenoko <75839443+Amdsnk@users.noreply.github.com>
2025-03-04 07:37:14 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "https://scraping-ai-chat-production.up.railway.app/",
+        source: '/api/:path*',
+        destination: 'https://scraping-ai-chat-production.up.railway.app/',
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
-
+export default nextConfig;
