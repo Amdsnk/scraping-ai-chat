f5b455b62b3b52e929bbb455ecc26aede3d93b01
Amanda Angela Soenoko <75839443+Amdsnk@users.noreply.github.com>
2025-03-04 07:30:51 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://scraping-ai-chat-production.up.railway.app/",
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
+
